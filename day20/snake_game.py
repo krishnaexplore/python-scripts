9eb7df5bcfead6af185c277cda165870d42ba553
Krishna Gangaraju <37485969+krishnaexplore@users.noreply.github.com>
2023-08-26 10:24:35 +0530

    Create snake_game.py

---
+++ day20/snake_game.py
@@ -0,0 +1,12 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake game")
+
+tim = Turtle(shape="square")
+
+
+
+screen.exitonclick()
